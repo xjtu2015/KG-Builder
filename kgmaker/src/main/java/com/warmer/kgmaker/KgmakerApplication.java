9c80f78ca19e13c7d2b4916505341d82c1909bdd
chenH <490726782@qq.com>
2020-01-08 22:59:54 +0800

    初始化

---
+++ kgmaker/src/main/java/com/warmer/kgmaker/KgmakerApplication.java
@@ -0,0 +1,13 @@
+package com.warmer.kgmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan
+public class KgmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgmakerApplication.class, args);
+	}
+}
